48a18e2594fbf44b74f0b6f3fc196daac5b88d84
SmartGuyz <a.gerritsen@sourcexs.nl>
2018-02-05 16:17:52 +0100

    Ignore changes to config.ini

--- tester.php
+++ tester.php
@@ -1,11 +1,17 @@
 <?php
-//$aPostFiels['data'] = "test";
+$oData = new stdClass();
+$oData->vdm = 12345678;
+$oData->relation = 12345678;
+$oData->blaat = "tester";
+
+
+$aPostFiels['data'] = ["toAddress" => "test", "dataObject" => serialize($oData)];
 
 $rCurl = curl_init();
 
-curl_setopt($rCurl, CURLOPT_URL,"http://phpbc.sourcexs.nl:3001/address");
+curl_setopt($rCurl, CURLOPT_URL,"http://phpbc.sourcexs.nl:3001/sendTransaction");
 curl_setopt($rCurl, CURLOPT_POST, false);
-//curl_setopt($rCurl, CURLOPT_POSTFIELDS, json_encode($aPostFiels));
+curl_setopt($rCurl, CURLOPT_POSTFIELDS, json_encode($aPostFiels));
 curl_setopt($rCurl, CURLOPT_RETURNTRANSFER, true);
 
 $sOutput = curl_exec($rCurl);
